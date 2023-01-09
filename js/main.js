7126aeec4935e68645227347854dd7453f153d12
Anthony Martin <anthonymartin20@gmail.com>
2023-01-09 10:10:04 +0100

    Refactoring draw function

--- js/main.js
+++ js/main.js
@@ -1,68 +1,66 @@
 
-    const domBookList = document.querySelector('#book-list');
-    
-    const inputTitle = document.querySelector('#input-title');
-    const inputAuthor = document.querySelector('#input-author');
-    const buttonAdd = document.querySelector('#button-add');
-    let bookList = [
-        
-    ];
-    
-    console.log(bookList)
-    const stringBookList = localStorage.getItem('bookList');
-    if(stringBookList){
-        bookList = JSON.parse(stringBookList);
-        console.log(bookList)
-    } 
-    console.log(bookList)
-    drawBooks();
-    console.log(bookList)
+const domBookList = document.querySelector('#book-list');
+const inputTitle = document.querySelector('#input-title');
+const inputAuthor = document.querySelector('#input-author');
+const buttonAdd = document.querySelector('#button-add');
 
+const stringBookList = localStorage.getItem('bookList');
+
+let bookList = [];
+
+if(stringBookList){
+  const parsedBookList = JSON.parse(stringBookList);
+
+  if(Array.isArray(parsedBookList)){
+    bookList = parsedBookList;
+  }
+} 
+
+drawBooks();
 
 function addBook (title, author) {
-    if(title && author){
-        bookList.push({title, author});
-        drawBooks();
-    }
+  if(title && author){
+    bookList.push({title, author});
+    drawBooks();
+  }
 }
 
 function removeBook (index) {
-    bookList.splice(index, 1);
-    drawBooks();
+  bookList.splice(index, 1);
+  drawBooks();
 }
 
 function drawBooks() {
-    let innerHtml = '';
-    
-    bookList.forEach((book, index) => {
-        innerHtml += `
-        <li>
-            <h3>${book.title}</h3>
-            ${book.author}
-            <button id="remove_btn_${index}">Remove</button>
-        </li>
-        `;
+  let innerHtml = '';
+  
+  bookList.forEach((book, index) => {
+    innerHtml += `
+    <li>
+      <h3>${book.title}</h3>
+      ${book.author}
+      <button id="remove_btn_${index}">Remove</button>
+    </li>
+    `;
+  });
+  
+  domBookList.innerHTML = innerHtml;
+
+  bookList.forEach((book, index) =>{
+    const removeBtn = document.querySelector(`#remove_btn_${index}`)
+    removeBtn.addEventListener('click', (e) =>{
+      removeBook(index);
     });
-    
-    domBookList.innerHTML = innerHtml;
+  });
 
-    bookList.forEach((book, index) =>{
-        const removeBtn = document.querySelector(`#remove_btn_${index}`)
-        removeBtn.addEventListener('click', (e) =>{
-            removeBook(index)
-        })
-    })
-
-    localStorage.setItem('bookList', JSON.stringify(bookList))
-    console.log(JSON.stringify(bookList))
-
+  localStorage.setItem('bookList', JSON.stringify(bookList));
 }
 
 
 buttonAdd.addEventListener('click', (event) => {
-    event.preventDefault();
-    addBook (inputTitle.value, inputAuthor.value);
-    inputTitle.value = '';
-    inputAuthor.value = '';
+  event.preventDefault();
+  addBook (inputTitle.value, inputAuthor.value);
+
+  inputTitle.value = '';
+  inputAuthor.value = '';
 })
 
