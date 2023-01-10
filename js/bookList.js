9c83e9a2d2b30e9effd73953ade4bf0da0e0434c
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-01-10 14:45:55 +0630

    added drawBooks method in class

--- js/bookList.js
+++ js/bookList.js
@@ -1,20 +1,53 @@
 import Book from './book.js';
 
 export default class BookList {
-    constructor(){
-        this.bookList = [];
-        this.domBookList = document.querySelector('#book-list');
-        this.inputTitle = document.querySelector('#input-title');
-        this.inputAuthor = document.querySelector('#input-author');
-        this.buttonAdd = document.querySelector('#button-add');
-    }
+  constructor() {
+    this.bookList = [];
+    this.domBookList = document.querySelector('#book-list');
+    this.inputTitle = document.querySelector('#input-title');
+    this.inputAuthor = document.querySelector('#input-author');
+    this.buttonAdd = document.querySelector('#button-add');
 
-    addBook(title, author){
-        const myBook = new Book(title, author);
-        this.bookList.unshift(myBook);
-    }
+    this.buttonAdd.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.addBook(this.inputTitle.value, this.inputAuthor.value);
+      
+      this.inputTitle.value = '';
+      this.inputAuthor.value = '';
+    });
+    this.drawBooks();
+  }
 
-    removeBook(index) {
-        this.bookList.splice(index, 1);
-    }
+  addBook(title, author) {
+    const myBook = new Book(title, author);
+    this.bookList.unshift(myBook);
+    this.drawBooks();
+  }
+
+  removeBook(index) {
+    this.bookList.splice(index, 1);
+    this.drawBooks();
+  }
+
+  drawBooks() {
+    this.domBookList.innerHTML = '';
+
+    this.bookList.forEach((book, index) => {
+      const newLi = document.createElement('li');
+      newLi.innerHTML = `
+            <li>
+              <h3>${book.title}</h3>
+              ${book.author}
+              <button id="remove_btn_${index}">Remove</button>
+            </li>
+            `;
+
+      this.domBookList.appendChild(newLi);
+      const removeBtn = document.querySelector(`#remove_btn_${index}`);
+      removeBtn.addEventListener('click', () => {
+        this.removeBook(index);
+        this.drawBooks();
+      });
+    });
+  }
 }
